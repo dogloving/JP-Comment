d74b97f380f2ccb94a625f83e494ff30403a4905
huangjingping <1304272317@qq.com>
2017-10-03 22:47:20 +0800

    Amend db process.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+echo phpinfo();
 class MyDB extends SQLite3
    {
       function __construct()
@@ -15,19 +16,19 @@ class MyDB extends SQLite3
 
    $sql =<<<EOF
       CREATE TABLE SITE(
-	URL VARCHAR(200) PRIMARY KEY     NOT NULL,
-	ORIGIN VARCHAR(100) NOT NULL,
-	NUMBER INT NOT NULL);
+		URL VARCHAR(200) PRIMARY KEY     NOT NULL,
+		ORIGIN VARCHAR(100) NOT NULL,
+		NUMBER INT NOT NULL);
       CREATE TABLE USER(
-	UID VARCHAR(48) PRIMARY KEY NOT NULL,
-	NICKNAME VARCHAR(30) NOT NULL,
-	HEADICON VARCHAR(200));
+		UID VARCHAR(48) PRIMARY KEY NOT NULL,
+		NICKNAME VARCHAR(30) NOT NULL,
+		HEADICON VARCHAR(200));
       CREATE TABLE COMMENTS(
-	CID VARCHAR(48) PRIMARY KEY NOT NULL,
-	URL VARCHAR(48) NOT NULL,
-	USER VARCHAR(48) NOT NULL,
-	DATE VARCHAR(20) NOT NULL,
-	CONTENT TEXT);
+		CID VARCHAR(48) PRIMARY KEY NOT NULL,
+		URL VARCHAR(48) NOT NULL,
+		USER VARCHAR(48) NOT NULL,
+		DATE VARCHAR(20) NOT NULL,
+		CONTENT TEXT);
 EOF;
    $ret = $db->exec($sql);
    if(!$ret){
@@ -35,6 +36,7 @@ EOF;
    } else {
       echo "Table created successfully\n";
    }
+   
    $sql = sprintf("select * from SITE where URL = '%s'", 'site');
    $ret = $db->query($sql);
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
--- models/db.php
+++ models/db.php
@@ -1,15 +1,43 @@
 <?php 
-	function getHandler(){
-		$host = 'localhost';
-		$database = 'comments';
-		$username = 'root';
-		$password = '';
-		$pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$pdo->exec('set names "utf8"');
-		return $pdo;
-	}
+	class DB extends SQLite3 {
+      	function __construct() {
+         	$this->open('comments.db');
+      	}
+   	}
+   	$db = new DB();
+   	if(!$db) {
+   		echo '数据库出错';
+   	}
 
+   	/**
+   	*	在数据库中建表
+   	*/
+   	function createTable() {
+$sql =<<<EOF
+    CREATE TABLE SITE(
+	URL VARCHAR(200) PRIMARY KEY     NOT NULL,
+	ORIGIN VARCHAR(100) NOT NULL,
+	NUMBER INT NOT NULL);
+    CREATE TABLE USER(
+	UID VARCHAR(48) PRIMARY KEY NOT NULL,
+	NICKNAME VARCHAR(30) NOT NULL,
+	HEADICON VARCHAR(200));
+    CREATE TABLE COMMENTS(
+	CID VARCHAR(48) PRIMARY KEY NOT NULL,
+	URL VARCHAR(48) NOT NULL,
+	USER VARCHAR(48) NOT NULL,
+	DATE VARCHAR(20) NOT NULL,
+	CONTENT TEXT);
+EOF;
+	  	$ret = $db->exec($sql);
+	   	if(!$ret) {
+	    	return true;
+	   	} else {
+	    	return false;
+	   	}
+   	}
+
+   	createTable();
 	/**
 	*	将新评论存入数据库中
 	*	@param nickname string 评论者昵称
@@ -20,47 +48,43 @@
 	*	@return result 存储反馈
 	*/
 	function save($nickname='', $site='', $content='', $origin='', $url='') {
-		$pdo = getHandler();
-		try {
-			// 检查表Site
-			$sql = sprintf("SELECT * FROM Site WHERE url = ''", $url);
-			$site = $pdo->query($sql);
-			if($site->rowCount()) {
-				foreach ($site as $row) {
-					$number = $row['number'];
-				}
-				$sql = sprintf("UPDATE Site SET number = '%d'", $number + 1);
-				$pdo->query($sql);
-			} else {
-				$number = 1;
-				$sql = sprintf("INSERT INTO Site VALUES('%s', '%s', '%s')", $url, $origin, $number);
-			}
-
-			// 检查User
-			$uid = md5($nickname.$site);
-			$sql = sprintf("SELECT * FROM User WHERE uid = '%s'", $uid);
-			$user = $pdo->query($sql);
-			if($user->rowCount()) {
-				foreach ($user as $row) {
-					$headicon = $row['headicon'];
-				}
-			} else {
-				$headicons = array('1', '2', '3');
-				$headicon = $headicons[rand(0, count($headicons) - 2)];
-				$sql = sprintf("INSERT INTO User VALUES('%s', '%s', '%s')", $uid, $nickname, $headicon);
-			}
-
-			// 检查Comment
-			$sql = sprintf("INSERT INTO Comment VALUES('%s', '%s', '%s', '%s', '%s')", $cid, $url, $uid, $date, $content);
-			$comment = $pdo->query($sql);
-			if($comment.rowCount()) {
-				return array(1, array('nickname' => $nickname, 'headicon' => $headicon, 'content' => $content));
-			} else {
-				return array(0);
-			}
-		} catch(PDOException $e) {
-			return array(-1, $e->getMessage());
+		// 检查表Site
+		$sql = sprintf("SELECT * FROM SITE WHERE URL = ''", $url);
+		$site = $db->query($sql);
+		$count = 0;
+		while($row = $site->fetchArray(SQLITE3_ASSOC)) {
+			$count++;			
+			$sql = sprintf("UPDATE SITE SET NUMBER = '%d'", $number + 1);
+			$db->query($sql);
 		}
+		if($count == 0) {
+			$number = 1;
+			$sql = sprintf("INSERT INTO SITE VALUES('%s', '%s', '%d')", $url, $origin, $number);
+		}
+
+		// 检查User
+		$uid = md5($nickname.$site);
+		$sql = sprintf("SELECT * FROM USER WHERE UID = '%s'", $uid);
+		$user = $db->query($sql);
+		$count = 0;
+		while($row = $user->fetchArray(SQLITE3_ASSOC)) {
+			$count++;
+			$headicon = $row['HEADICON'];
+		}
+		if($count == 0) {
+			$headicons = array('1', '2', '3');
+			$headicon = $headicons[rand(0, count($headicons) - 2)];
+			$sql = sprintf("INSERT INTO USER VALUES('%s', '%s', '%s')", $uid, $nickname, $headicon);
+		}
+
+		// 检查Comment
+		$sql = sprintf("INSERT INTO COMMENT VALUES('%s', '%s', '%s', '%s', '%s')", $cid, $url, $uid, $date, $content);
+		$comment = $db->query($sql);
+		$count = 0;
+		while($row = $comment->fetchArray(SQLITE3_ASSOC)) {
+			return array(1, array('nickname' => $nickname, 'headicon' => $headicon, 'content' => $content));
+		}
+		return array(0);
 	}
 
 	/**
@@ -69,20 +93,17 @@
 	*	@return comments array 该url下的所有评论
 	*/
 	function get($url='') {
-		$pdo = getHandler();
+		$db = getHandler();
 		return array(0);
 		try {
-			$sql = sprintf("SELECT nickname, headicon, date, content FROM Comment, User WHERE Comment.user = User.uid AND url = '%s' ORDER BY date", $url);
-			$comment = $pdo->query($sql);
+			$sql = sprintf("SELECT NICKNAME, HEADICON, DATE, CONTENT FROM COMMENT, USER WHERE COMMENT.USER = USER.UID AND URL = '%s' ORDER BY DATE", $url);
+			$comment = $db->query($sql);
 			$result = array();
-			if($comment.rowCount()) {
-				foreach ($comment as $row) {
-					$array_push($result, array('nickname' => $row['nickname'], 'headicon' => $row['headicon'], 'date' => $row['date'], 'content' => $row['content']));
-				}
-				return array(1, $result);
+			while($row = $comment->fetchArray(SQLITE3_ASSOC)) {
+				$array_push($result, array('nickname' => $row['NICKNAME'], 'headicon' => $row['HEADICON'], 'date' => $row['DATE'], 'content' => $row['CONTENT']));
 			}
-			return array(0);
-		} catch(PDOException $e) {
+			return array(1, $result);
+		} catch(dbException $e) {
 			return array(-1, $e->getMessage());
 		}
 	}
